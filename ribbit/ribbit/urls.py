86e63a6aec0b3c7a0a1604c49706ea938ca41de0
Shubham7393 <shubham731993@gmail.com>
2015-01-24 18:35:39 +0530

    Ribbit

---
+++ ribbit/ribbit/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ribbit.views.home', name='home'),
+    # url(r'^ribbit/', include('ribbit.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'ribbit_app.views.index'), # root
+    url(r'^login$', 'ribbit_app.views.login_view'), # login
+    url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
+    url(r'^signup$', 'ribbit_app.views.signup'), # signup
+    url(r'^ribbits$', 'ribbit_app.views.public'), # public ribbits
+    url(r'^submit$', 'ribbit_app.views.submit'), # submit new ribbit
+    url(r'^users/$', 'ribbit_app.views.users'),
+    url(r'^users/(?P<username>\w{0,30})/$', 'ribbit_app.views.users'),
+    url(r'^follow$', 'ribbit_app.views.follow'),
+)
